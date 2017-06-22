4676fd91a6c97113baddaacb6a23803a650722c9
chenyi1976 <yichen1976@gmail.com>
2017-06-22 16:31:20 +1000

    carousel indicator support

--- script/rss.carousel.js
+++ script/rss.carousel.js
@@ -14,8 +14,9 @@ function generateCarousel() {
     $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent('https://www.ozbargain.com.au/deals/feed') + '&callback=?', function (data) {
         // $("#deal_content").html('');
         $("#carousel-inner").html('');
+        $("#carousel-indicators").html('');
 
-        var items = $($.parseXML( data.contents )).find('item')
+        var items = $($.parseXML(data.contents)).find('item')
 
         $.each(items, function (i, field) {
             // $("#deal_content").append('<li>' + field.children[0].textContent + '</li>');
@@ -24,15 +25,21 @@ function generateCarousel() {
                     + field.children[0].textContent + '</div><div class="item_link">'
                     + '<a href="' + field.children[1].textContent + '"> ' + field.children[1].textContent + '</a></div><div class="item_description">'
                     + field.children[2].textContent + '</div></div>');
+                $("#carousel-indicators").append('<li data-target="#myCarousel" data-slide-to="0" class="active"></li>');
+
             } else {
                 $("#carousel-inner").append('<div class="item"><div class="item_title">'
                     + field.children[0].textContent + '</div><div class="item_link">'
                     + '<a href="' + field.children[1].textContent + '"> ' + field.children[1].textContent + '</a></div><div class="item_description">'
                     + field.children[2].textContent + '</div></div>');
+                $("#carousel-indicators").append('<li data-target="#myCarousel" data-slide-to="' + i + '"></li>');
             }
         });
 
         $("#myCarousel").carousel();
+        $(".item").click(function(){
+            $("#myCarousel").carousel(1);
+        });
         $("#progress-bar").text("Refreshed at " + moment().format("h:mm:ss"));
     });
 }
